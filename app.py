0ff3d1c8d84db43f00e9744d96f5627869be3d69
Alex Chan <alex@alexwlchan.net>
2020-04-01 23:15:00 +0100

    Get some graph stuff working

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ from functools import reduce
 from operator import or_
 
 import networkx as nx
+from networkx.readwrite import json_graph
 
 G = nx.Graph()
 
@@ -44,6 +45,81 @@ def get_nodes_within_distance(G, *, node, max_distance=2):
 
     return result
 
+
+def write_html(f_name):
+    result = '''<script src="http://d3js.org/d3.v2.min.js?2.9.3"></script>
+<style>
+.link {
+  stroke: #aaa;
+}
+.node text {
+stroke:#333;
+cursos:pointer;
+}
+.node circle{
+stroke:#fff;
+stroke-width:3px;
+fill:#555;
+}
+</style>
+<body>
+<script>
+var width = 300,
+    height = 250
+var svg = d3.select("body").append("svg")
+    .attr("width", width)
+    .attr("height", height);
+var force = d3.layout.force()
+    .gravity(.1)
+    .distance(100)
+    .charge(-100)
+    .size([width, height]);
+'''
+    result += 'd3.json("' + f_name + '", function(json) {'
+    result += '''force
+      .nodes(json.nodes)
+      .links(json.links)
+      .start();
+  var link = svg.selectAll(".link")
+      .data(json.links)
+    .enter().append("line")
+      .attr("class", "link")
+    .style("stroke-width", function(d) { return Math.sqrt(d.weight); });
+  var node = svg.selectAll(".node")
+      .data(json.nodes)
+    .enter().append("g")
+      .attr("class", "node")
+      .call(force.drag);
+  node.append("circle")
+      .attr("r","5");
+  node.append("text")
+      .attr("dx", 12)
+      .attr("dy", ".35em")
+      .text(function(d) { return d.name });
+  force.on("tick", function() {
+    link.attr("x1", function(d) { return d.source.x; })
+        .attr("y1", function(d) { return d.source.y; })
+        .attr("x2", function(d) { return d.target.x; })
+        .attr("y2", function(d) { return d.target.y; });
+    node.attr("transform", function(d) { return "translate(" + d.x + "," + d.y + ")"; });
+  });
+});
+</script>
+'''
+    return result
+
+
+
 from pprint import pprint
 
-pprint(get_nodes_within_distance(G, node="口"))
+# pprint(get_nodes_within_distance(G, node="口"))
+
+print(write_html("data.json"))
+
+# print(json_graph.node_link_data(G))
+
+
+
+
+
+
