7fbf37c2557e2c5ff3c84c4d8112ba209971e38f
Alex Chan <alex@alexwlchan.net>
2020-04-02 07:20:13 +0100

    Add comments, dependencies

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 
+import functools
 from functools import reduce
 from operator import or_
 
@@ -25,23 +26,30 @@ G.add_edge("猫", "喵")
 G.add_edge("猫", "狗")
 G.add_edge("喝", "渴")
 
-def get_nodes_within_distance(G, *, node, max_distance=2):
+
+@functools.lru_cache()
+def get_nodes_within_distance(graph, *, node, max_distance=2):
     """
     Get the nodes within a given distance of the selected node.
 
     e.g. if ``max_distance`` is 2, it would find all nodes that are at most
     two steps away from ``node``.
     """
+    # Distance 0 from the target node is only the node itself.
     seen = set(node)
     result = {0: set(node)}
 
-    for dist in range(1, max_distance + 1):
+    for dist in range(max_distance):
+        # Find all the direct neighbours of anything that's ``dist``
+        # from the central node, and which we haven't seen before.
         new_neighbours = reduce(or_, [
-            set(G.neighbors(node)) for node in result[dist - 1]
+            set(graph.neighbors(node)) for node in result[dist]
         ]) - seen
 
+        # Anything that's one away from ``dist`` is ``dist + 1`` away
+        # from the central node.
+        result[dist + 1] = new_neighbours
         seen |= new_neighbours
-        result[dist] = new_neighbours
 
     return result
 
